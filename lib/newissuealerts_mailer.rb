862fda018ff32ecd824addf3dade542e28c1eaf4
Emmanuel Bretelle <chantra@rahu.(none)>
2009-07-30 16:08:11 +0200

    Attempting to get it work with rails 2.2.2/2.1.2 and redmine svn/0.8.4

--- lib/newissuealerts_mailer.rb
+++ lib/newissuealerts_mailer.rb
@@ -11,9 +11,15 @@ class NewissuealertsMailer < Mailer
                     'Issue-Author' => author.login
     redmine_headers 'Issue-Assignee' => issue.assigned_to.login if issue.assigned_to
 
+    content_type "multipart/alternative"
+
     recipients email
     subject "[#{issue.project.name} - #{issue.tracker.name} ##{issue.id}] (#{issue.status.name}) #{issue.subject}"
-    body :issue => issue,
+    body = {
+         :issue => issue,
          :issue_url => url_for(:controller => 'issues', :action => 'show', :id => issue)
+    }
+    part :content_type => "text/plain", :body => render_message("newissuealert.text.plain.rhtml", body)
+    part :content_type => "text/html", :body => render_message("newissuealert.text.html.rhtml", body)
   end
 end
